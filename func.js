6957adcd22db29af42bb619374d22987abaeadab
Mahadi176 <mahadi210176@gmail.com>
2026-02-26 23:47:05 +0600

    some updates

--- func.js
+++ func.js
@@ -2,6 +2,20 @@ function add(num1,num2){
     const sum = num1 +num2 ;
     return sum;
 }
-
 const result = add(2,3);
 console.log(result)
+
+function subs(num1,num2){
+    const sub = num1 - num2 ;
+    return sub;
+}
+const result1 = subs(2,3);
+
+console.log(result1)
+
+function mult(num1,num2){
+    const mul = num1*num2 ;
+    return mul;
+}
+const result2 = mult(2,3);
+console.log(result2)
