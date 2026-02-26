7df246642b083199d213ce58bf3fa2c2484a9245
Mahadi176 <mahadi210176@gmail.com>
2026-02-27 04:07:01 +0600

    pushhhhhhhhhhhhhhhhhhh

--- arrow.js
+++ arrow.js
@@ -7,3 +7,9 @@ const multiply = (num1,num2) => num1*num2 ;
 console.log('sum',add(2,3))
 
 console.log('multiply',multiply(3,4))
+
+
+document.getElementById('btn').addEventListener('click', (event) => {
+        event.target.parentNode.innerText
+
+})
