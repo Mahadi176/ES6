5b1eae3cfcd6793672a300160033b739ed3033e9
Mahadi176 <mahadi210176@gmail.com>
2026-03-02 00:26:01 +0600

    jhanku

--- map.js
+++ map.js
@@ -11,3 +11,14 @@ const friends = ['Abed','Babed','Caved','Dabed'];
 const firstLetter = friends.map(frd => frd[0]);
 
 console.log(firstLetter)
+
+const products = [
+    { name : 'xiaomi', price : 12000},
+    { name : 'vivo', price : 22000},
+    { name : 'samsung', price : 42000},
+    { name : 'apple', price : 72000},
+    { name : 'htc', price : 32000}                    
+ ]
+
+ const price = products.map(pd => pd.price);
+ console.log(price)
