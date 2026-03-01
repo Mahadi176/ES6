b128e1db6d450a250011c4e6d3dd3149b69e50fe
Mahadi176 <mahadi210176@gmail.com>
2026-03-02 00:14:59 +0600

    push

--- map.js
+++ map.js
@@ -1,4 +1,7 @@
 const number = [1,2,3,4]
 
 const doubled = number.map(number => number*2)
+const squared = number.map(number => number*number)
 console.log(doubled)
+console.log(squared)
+console.log(number.length*squared.length)
